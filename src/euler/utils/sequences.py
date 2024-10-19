a940dfbaa0da623b85ba9844073a7b7074b6c4cd
pjd199 <89210268+pjd199@users.noreply.github.com>
2024-10-19 20:26:33 +0000

    solved

--- src/euler/utils/sequences.py
+++ src/euler/utils/sequences.py
@@ -1,5 +1,6 @@
 from collections.abc import Iterator
 from itertools import count
+from math import sqrt
 
 
 def fibonacci() -> Iterator[int]:
@@ -17,6 +18,23 @@ def triangular() -> Iterator[int]:
         yield (n * (n + 1)) // 2
 
 
+def is_triagular(n: int) -> bool:
+    return ((sqrt(8 * n + 1) - 1) / 2).is_integer()
+
+
 def pentagonal() -> Iterator[int]:
     for n in count(1):
         yield (n * (3 * n - 1)) // 2
+
+
+def is_pentagonal(n: int) -> bool:
+    return ((sqrt(24 * n + 1) + 1) / 6).is_integer()
+
+
+def hexagonal() -> Iterator[int]:
+    for n in count(1):
+        yield n * (2 * n - 1)
+
+
+def is_hexagonal(n: int) -> bool:
+    return ((sqrt(8 * n + 1) + 1) / 4).is_integer()
