de4cd4faeeb44061ba6b7371421a93488a6761a2
pjd199 <89210268+pjd199@users.noreply.github.com>
2024-11-24 16:22:06 +0000

    solved

---
+++ src/euler/problems/problem0072.py
@@ -0,0 +1,28 @@
+"""
+Project Euler Problem 72
+========================
+
+Consider the fraction, n/d, where n and d are positive integers. If n < d
+and HCF(n,d)=1, it is called a reduced proper fraction.
+
+If we list the set of reduced proper fractions for d 8 in ascending order
+of size, we get:
+
+1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3,
+                       5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+
+It can be seen that there are 21 elements in this set.
+
+How many elements would be contained in the set of reduced proper
+fractions for d 1,000,000?
+"""
+
+from euler.utils.euler_totient import totients_sieve
+
+
+def solution0072() -> int:
+    return sum(totients_sieve(1000000)) - 2
+
+
+if __name__ == "__main__":
+    print(solution0072())
